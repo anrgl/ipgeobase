ec43c37748b99ec855953f701df68b56f45e3627
@hexlet/cli <support@hexlet.io>
2021-10-20 20:42:09 +0500

    @hexlet/cli: submit

--- exercises/testing/test/stack_test.rb
+++ exercises/testing/test/stack_test.rb
@@ -3,39 +3,45 @@
 require_relative 'test_helper'
 require_relative '../lib/stack'
 
+# StackTest class
 class StackTest < Minitest::Test
-  def setup
-    @stack = Stack.new
-  end
+  # def setup
+  #   @stack = Stack.new
+  # end
 
-  def teardown
-    @stack.clear!
-  end
+  # def teardown
+  #   @stack.clear!
+  # end
 
   def test_stack_is_empty
-    assert @stack.empty?
+    stack = Stack.new
+    assert stack.empty?
   end
 
   def test_stack_push_one_element
-    @stack.push!(1)
-    assert_equal @stack.size, 1
+    stack = Stack.new.push!(1)
+    assert_equal stack.size, 1
   end
 
-  def test_stack_two_elements
-    @stack.push!(1)
-    @stack.push!(2)
-    assert_equal @stack.size, 2
+  def test_stack_new
+    stack = Stack.new([1, 2])
+    assert_equal stack.size, 2
   end
 
   def test_stack_pop_top_element
-    @stack.push!(1)
-    @stack.push!(2)
-    assert_equal @stack.pop!, 2
-    assert_equal @stack.size, 1
+    stack = Stack.new([1, 2])
+    assert_equal stack.pop!, 2
+    assert_equal stack.to_a, Stack.new([1]).to_a
+  end
+
+  def test_empty_stack_pop
+    stack = Stack.new
+    assert_nil stack.pop!, nil
   end
 
   def test_stack_to_array
-    assert @stack.to_a.is_a?(Array)
+    stack = Stack.new
+    assert stack.to_a.is_a?(Array)
   end
 end
 
