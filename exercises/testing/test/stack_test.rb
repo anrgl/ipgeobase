fbf00a233ba05612ecd09fc17604b8ddedc46ff7
@hexlet/cli <support@hexlet.io>
2021-11-24 00:28:22 +0500

    @hexlet/cli: submit

--- exercises/testing/test/stack_test.rb
+++ exercises/testing/test/stack_test.rb
@@ -12,28 +12,34 @@ class StackTest < Minitest::Test
 
   def test_stack_push_one_element
     stack = Stack.new.push!(1)
-    assert_equal stack.size, 1
+    assert_equal stack.to_a, [1]
   end
 
   def test_stack_new
     stack = Stack.new([1, 2])
-    assert_equal stack.size, 2
+    assert_equal stack.to_a, [1, 2]
   end
 
   def test_stack_pop_top_element
     stack = Stack.new([1, 2])
     assert_equal stack.pop!, 2
-    assert_equal stack.to_a, Stack.new([1]).to_a
+    assert_equal stack.to_a, [1]
   end
 
   def test_empty_stack_pop
     stack = Stack.new
-    assert_nil stack.pop!, nil
+    assert_nil stack.pop!
   end
 
   def test_stack_to_array
     stack = Stack.new
     assert stack.to_a.is_a?(Array)
+  end
+
+  def test_stack_clear!
+    stack = Stack.new([1, 2])
+    stack.clear!
+    assert stack.empty?
   end
 end
 
